abf559a2722a6ade3168cb8ab44aec715a32e846
Welington Cruz de Assis <welingtoncassis@gmail.com>
2021-07-30 18:15:58 -0300

    feat: configuracao inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos' },
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular' },
+    { icon: 'wallet-outline', text: 'Depositar' },
+    { icon: 'options-outline', text: 'Ajustar limute' },
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'barcode-outline', text: 'Pagar' },
+    { icon: 'lock-open-outline', text: 'Bloquear cartão' },
+    { icon: 'card-outline', text: 'Cartão virtual' },
+  ];
+
   constructor() {}
 
 }
