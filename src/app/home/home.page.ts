c1f20f59641c75be9f06f02f885b60a55f672919
Welington Cruz de Assis <welingtoncassis@gmail.com>
2021-07-30 20:31:47 -0300

    feat(animacao): add animação de subir e descer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AnimationController, Animation, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  @ViewChild('blocks') blocks: any;
 
   public options: Array<any> = [
     { icon: 'person-add-outline', text: 'Indicar amigos' },
@@ -31,6 +34,36 @@ export class HomePage {
     { icon: 'phone-portrait-outline', text: 'Configurações do app' },
   ];
 
-  constructor() {}
+  // usada para saber se animação vai para cima ou para baixo
+  public initialStep = 0;
+  // usada para controlar o máximo de onde ir no eixo y na animação
+  private maxTranslate: number;
+  private animation: Animation;
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform
+  ) {
+    // pega a altura da tela e diminui o suficiente para não esconder todo o conteudo e poder arrastar para cima de volta
+    this.maxTranslate = this.platform.height() - 200;
+  }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngAfterViewInit() {
+    this.createAnimation();
+  }
+
+  toggleBlocks() {
+    this.initialStep = this.initialStep === 0 ? this.maxTranslate : 0;
+
+    this.animation.direction(this.initialStep === 0 ? 'reverse' : 'normal').play();
+  }
+
+  createAnimation() {
+    this.animation = this.animationCtrl.create()
+    .addElement(this.blocks.nativeElement)
+    .duration(300)
+    .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`);
+  }
 
 }
